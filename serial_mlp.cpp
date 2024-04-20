ec972da6fca0655d9117d2eaabbb30860d2b8f11
Evan Vera <erv29@cornell.edu>
2024-04-20 19:25:07 -0400

    Serial code now supports arbitrary number of layers and activation functions

--- serial_mlp.cpp
+++ serial_mlp.cpp
@@ -10,6 +10,10 @@
 #include <algorithm>
 #include <random>
 
+#include "activations.h"
+
+typedef double (*fun_t)(double);
+
 void hadamard_product(double *a, double *b, double *c, int n)
 {
     for (int i = 0; i < n; i++)
@@ -18,31 +22,19 @@ void hadamard_product(double *a, double *b, double *c, int n)
     }
 }
 
-inline double sigmoid(double x)
-{
-    return 1 / (1 + exp(-x));
-}
-
-inline double d_sigmoid(double x)
-{
-    return x * (1 - x);
-}
-
 typedef struct mlp
 {
-    int input_size;
-    int hidden_size;
-    int output_size;
+    int num_layers;
+    int *layer_sizes;
 
-    double *hidden_weights;
-    double *output_weights;
+    double **weights;
+    double **biases;
 
-    double *hidden_bias;
-    double *output_bias;
+    fun_t *activations;
+    fun_t *d_activations;
 
-    double *input_layer;
-    double *hidden_layer;
-    double *output_layer;
+    double **layers;
+    double **deltas;
 } mlp_t;
 
 double *rand_vector(int n)
@@ -60,95 +52,6 @@ double *rand_vector(int n)
     return v;
 }
 
-mlp_t *create_mlp(int input_size, int hidden_size, int output_size)
-{
-    mlp_t *mlp = (mlp_t *)malloc(sizeof(mlp_t));
-
-    mlp->input_size = input_size;
-    mlp->hidden_size = hidden_size;
-    mlp->output_size = output_size;
-
-    mlp->hidden_weights = rand_vector(input_size * hidden_size);
-    mlp->output_weights = rand_vector(hidden_size * output_size);
-
-    mlp->hidden_bias = rand_vector(hidden_size);
-    mlp->output_bias = rand_vector(output_size);
-
-    mlp->hidden_layer = (double *)malloc(hidden_size * sizeof(double));
-    mlp->output_layer = (double *)malloc(output_size * sizeof(double));
-
-    return mlp;
-}
-
-void mlp_forward(mlp_t *mlp, double *x)
-{
-    mlp->input_layer = x;
-
-    // h = W_h x
-    cblas_dgemv(CblasRowMajor, CblasNoTrans, mlp->hidden_size, mlp->input_size, 1.0, mlp->hidden_weights, mlp->input_size, mlp->input_layer, 1, 0.0, mlp->hidden_layer, 1);
-    // h += b_h
-    cblas_daxpy(mlp->hidden_size, 1.0, mlp->hidden_bias, 1, mlp->hidden_layer, 1);
-    // h = sigmoid(h)
-    std::transform(mlp->hidden_layer, mlp->hidden_layer + mlp->hidden_size, mlp->hidden_layer, sigmoid);
-
-    // y_hat = W_o h
-    cblas_dgemv(CblasRowMajor, CblasNoTrans, mlp->output_size, mlp->hidden_size, 1.0, mlp->output_weights, mlp->hidden_size, mlp->hidden_layer, 1, 0.0, mlp->output_layer, 1);
-    // y_hat += b_o
-    cblas_daxpy(mlp->output_size, 1.0, mlp->output_bias, 1, mlp->output_layer, 1);
-    // y_hat = sigmoid(y_hat)
-    std::transform(mlp->output_layer, mlp->output_layer + mlp->output_size, mlp->output_layer, sigmoid);
-}
-
-void mlp_backprop(mlp_t *mlp, double *y, double alpha)
-{
-    double *delta_o = (double *)malloc(mlp->output_size * sizeof(double));
-    double *delta_h = (double *)malloc(mlp->hidden_size * sizeof(double));
-
-    // delta_o = y - y_hat
-    cblas_dcopy(mlp->output_size, y, 1, delta_o, 1);
-    cblas_daxpy(mlp->output_size, -1.0, mlp->output_layer, 1, delta_o, 1);
-    // We overwrite our previous output with the derivative of sigmoid applied to it
-    std::transform(mlp->output_layer, mlp->output_layer + mlp->output_size, mlp->output_layer, d_sigmoid);
-    // delta_o = delta_o * d_sigmoid(output)
-    hadamard_product(delta_o, mlp->output_layer, delta_o, mlp->output_size);
-
-    // delta_h = W_o^T delta_o
-    cblas_dgemv(CblasRowMajor, CblasTrans, mlp->hidden_size, mlp->output_size, 1.0, mlp->output_weights, mlp->hidden_size, delta_o, 1, 0.0, delta_h, 1);
-    // We overwrite the hidden output with the derivative of sigmoid applied to it
-    std::transform(mlp->hidden_layer, mlp->hidden_layer + mlp->hidden_size, mlp->hidden_layer, d_sigmoid);
-    // delta_h *= d_sigmoid(hidden)
-    hadamard_product(delta_h, mlp->hidden_layer, delta_h, mlp->hidden_size);
-
-    // W_o += alpha * delta_o h^T
-    cblas_dger(CblasRowMajor, mlp->output_size, mlp->hidden_size, alpha, delta_o, 1, mlp->hidden_layer, 1, mlp->output_weights, mlp->hidden_size);
-    // b_o += alpha * delta_o
-    cblas_daxpy(mlp->output_size, alpha, delta_o, 1, mlp->output_bias, 1);
-
-    // W_h += alpha * delta_h x^T
-    cblas_dger(CblasRowMajor, mlp->hidden_size, mlp->input_size, alpha, delta_h, 1, mlp->input_layer, 1, mlp->hidden_weights, mlp->input_size);
-    // b_h += alpha * delta_h
-    cblas_daxpy(mlp->hidden_size, alpha, delta_h, 1, mlp->hidden_bias, 1);
-
-    fprintf(stderr, "Completed backprop\n");
-
-    free(delta_h);
-    free(delta_o);
-}
-
-void delete_mlp(mlp_t *mlp)
-{
-    free(mlp->hidden_weights);
-    free(mlp->output_weights);
-
-    free(mlp->hidden_bias);
-    free(mlp->output_bias);
-
-    free(mlp->hidden_layer);
-    free(mlp->output_layer);
-
-    free(mlp);
-}
-
 void print_vector(double *v, int n)
 {
     for (int i = 0; i < n; i++)
@@ -158,42 +61,173 @@ void print_vector(double *v, int n)
     printf("\n");
 }
 
+mlp_t *create_mlp(int num_layers, int *layers, fun_t *activations, fun_t *d_activations)
+{
+    mlp_t *mlp = (mlp_t *)malloc(sizeof(mlp_t));
+
+    mlp->num_layers = num_layers;
+    mlp->layer_sizes = (int *)malloc(num_layers * sizeof(int));
+
+    mlp->weights = (double **)malloc((num_layers - 1) * sizeof(double *));
+    mlp->biases = (double **)malloc((num_layers - 1) * sizeof(double *));
+
+    mlp->activations = activations;
+    mlp->d_activations = d_activations;
+
+    mlp->layers = (double **)malloc(num_layers * sizeof(double *));
+    mlp->deltas = (double **)malloc(num_layers * sizeof(double *));
+
+    for (int i = 0; i < num_layers; i++)
+    {
+        mlp->layer_sizes[i] = layers[i];
+
+        if (i < num_layers - 1)
+        {
+            mlp->weights[i] = rand_vector(layers[i + 1] * layers[i]);
+            mlp->biases[i] = rand_vector(layers[i + 1]);
+        }
+
+        mlp->layers[i] = (double *)malloc(layers[i] * sizeof(double));
+        mlp->deltas[i] = (double *)malloc(layers[i] * sizeof(double));
+    }
+
+    return mlp;
+}
+
+void mlp_forward(mlp_t *mlp, double *x)
+{
+    cblas_dcopy(mlp->layer_sizes[0], x, 1, mlp->layers[0], 1);
+
+    for (int i = 0; i < mlp->num_layers - 1; i++)
+    {
+        // h_{i + 1} = W_{h_i} h_{i}
+        cblas_dgemv(CblasRowMajor, CblasNoTrans, mlp->layer_sizes[i + 1], mlp->layer_sizes[i], 1.0, mlp->weights[i], mlp->layer_sizes[i], mlp->layers[i], 1, 0.0, mlp->layers[i + 1], 1);
+        // h_{i + 1} += b_{i}
+        cblas_daxpy(mlp->layer_sizes[i + 1], 1.0, mlp->biases[i], 1, mlp->layers[i + 1], 1);
+        // h_{i + 1} = sigmoid(h_{i + 1})
+        std::transform(mlp->layers[i + 1], mlp->layers[i + 1] + mlp->layer_sizes[i + 1], mlp->layers[i + 1], mlp->activations[i]);
+    }
+}
+
+void mlp_backprop(mlp_t *mlp, double *y, double alpha)
+{
+    int output_index = mlp->num_layers - 1;
+
+    // delta_o = y_hat
+    cblas_dcopy(mlp->layer_sizes[output_index], mlp->layers[output_index], 1, mlp->deltas[output_index], 1);
+    // delta_o -= y
+    cblas_daxpy(mlp->layer_sizes[output_index], -1.0, y, 1, mlp->deltas[output_index], 1);
+
+    for (int i = mlp->num_layers - 2; i >= 0; i--)
+    {
+        // We overwrite our previous output with the derivative of activation applied to it
+        std::transform(mlp->layers[i + 1], mlp->layers[i + 1] + mlp->layer_sizes[i + 1], mlp->layers[i + 1], mlp->d_activations[i]);
+        // delta_{i + 1} = delta_{i + 1} * d_activation(h_{i + 1})
+        hadamard_product(mlp->deltas[i + 1], mlp->layers[i + 1], mlp->deltas[i + 1], mlp->layer_sizes[i + 1]);
+
+        // W_i -= alpha * delta_{i + 1} h_i^T
+        cblas_dger(CblasRowMajor, mlp->layer_sizes[i + 1], mlp->layer_sizes[i], -1.0 * alpha, mlp->deltas[i + 1], 1, mlp->layers[i], 1, mlp->weights[i], mlp->layer_sizes[i]);
+
+        // b_i -= alpha * delta_{i + 1}
+        cblas_daxpy(mlp->layer_sizes[i + 1], -1.0 * alpha, mlp->deltas[i + 1], 1, mlp->biases[i], 1);
+
+        // delta_{i - 1} = W_i^T delta_{i + 1}
+        cblas_dgemv(CblasRowMajor, CblasTrans, mlp->layer_sizes[i + 1], mlp->layer_sizes[i], 1.0, mlp->weights[i], mlp->layer_sizes[i], mlp->deltas[i + 1], 1, 0.0, mlp->deltas[i], 1);
+    }
+}
+
+void delete_mlp(mlp_t *mlp)
+{
+    free(mlp->layer_sizes);
+
+    for (int i = 0; i < mlp->num_layers; i++)
+    {
+        if (i < mlp->num_layers - 1)
+        {
+            free(mlp->weights[i]);
+            free(mlp->biases[i]);
+        }
+
+        free(mlp->layers[i]);
+        free(mlp->deltas[i]);
+    }
+
+    free(mlp->weights);
+    free(mlp->biases);
+
+    free(mlp->layers);
+    free(mlp->deltas);
+
+    free(mlp);
+}
+
+void print_mlp(mlp_t *mlp)
+{
+    fprintf(stderr, "Input:\n");
+    print_vector(mlp->layers[0], mlp->layer_sizes[0]);
+
+    for (int i = 1; i < mlp->num_layers - 1; i++)
+    {
+        fprintf(stderr, "Hidden %d:\n", i);
+        print_vector(mlp->layers[i], mlp->layer_sizes[i]);
+    }
+
+    fprintf(stderr, "Output:\n");
+    print_vector(mlp->layers[mlp->num_layers - 1], mlp->layer_sizes[mlp->num_layers - 1]);
+
+    for (int i = 0; i < mlp->num_layers - 1; i++)
+    {
+        fprintf(stderr, "Weights %d:\n", i);
+        print_vector(mlp->weights[i], mlp->layer_sizes[i] * mlp->layer_sizes[i + 1]);
+
+        fprintf(stderr, "Biases %d:\n", i);
+        print_vector(mlp->biases[i], mlp->layer_sizes[i + 1]);
+
+        fprintf(stderr, "Deltas %d:\n", i);
+        print_vector(mlp->deltas[i], mlp->layer_sizes[i]);
+    }
+
+    fprintf(stderr, "Final Delta:\n");
+    print_vector(mlp->deltas[mlp->num_layers - 1], mlp->layer_sizes[mlp->num_layers - 1]);
+}
+
 int main()
 {
-    int input_size = 1;
-    int hidden_size = 8;
-    int output_size = 1;
+    int num_layers = 5;
+    int layers[] = {1, 8, 8, 8, 1};
 
-    mlp_t *mlp = create_mlp(input_size, hidden_size, output_size);
+    fun_t activations[] = {relu, relu, relu, identity};
+    fun_t d_activations[] = {d_relu, d_relu, d_relu, d_identity};
 
-    print_vector(mlp->hidden_weights, input_size * hidden_size);
-    print_vector(mlp->output_weights, hidden_size * output_size);
-
-    print_vector(mlp->hidden_bias, hidden_size);
-    print_vector(mlp->output_bias, output_size);
+    mlp_t *mlp = create_mlp(num_layers, layers, activations, d_activations);
 
     std::random_device rd;
     std::mt19937 gen(rd());
     std::normal_distribution<double> dist(0, 1);
 
-    for (int i = 0; i < 100000; i++)
+    for (int i = 0; i < 5000; i++)
     {
-        double x[1] = {dist(gen)};
-        double y[1] = {sin(x[0])};
+        double x = dist(gen);
+        double y = sin(x) * sin(x);
 
-        fprintf(stderr, "x: %f, y: %f\n", x[0], y[0]);
+        mlp_forward(mlp, &x);
+        mlp_backprop(mlp, &y, 0.0001);
 
-        fprintf(stderr, "Forward %d\n", i);
-        mlp_forward(mlp, x);
-        fprintf(stderr, "Output: %f\n", mlp->output_layer[0]);
-        fprintf(stderr, "Backprop %d\n", i);
-        mlp_backprop(mlp, y, 0.1);
+        print_mlp(mlp);
     }
 
-    double x[1] = {0.5};
-    mlp_forward(mlp, x);
+    std::ofstream file("data.csv");
 
-    fprintf(stderr, "Final output: %f\n", mlp->output_layer[0]);
+    for (int i = 0; i < 1000; i++)
+    {
+        double x = dist(gen);
+        double y = sin(x) * sin(x);
+
+        mlp_forward(mlp, &x);
+        file << x << "," << y << "," << mlp->layers[mlp->num_layers - 1][0] << std::endl;
+    }
+
+    file.close();
 
     delete_mlp(mlp);
 }
